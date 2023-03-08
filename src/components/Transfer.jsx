1dd25e924b6ec1696086aa4dc14caa6fd144c695
allentsangdev <allentsang04@gmail.com>
2023-03-08 18:44:09 -0500

    refactoring code base

--- src/components/Transfer.jsx
+++ src/components/Transfer.jsx
@@ -1,6 +1,18 @@
-import { useState, useRef } from "react"
-import TransferForm from "./TransferForm"
 import Receipt from "./Receipt"
+import { useState, useRef } from "react"
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
 
 const sourceAccount = "0x6dC70bEa16f1ef94A7350989ca5413a2E180860f"
 const destinationAccount = "0x03d0cf3f4A832C8E2c224BaA4a049110F39E630F"
@@ -17,21 +29,98 @@ const recieptData =
       gasUsed: "21000"
   }
 
-function Transfer() {
-    
-    // declare useRef hook to track the state change of the user input in the Amount text box
-    const valueRef = useRef(0)
-    // declare state variable to keep track to submited amount value
-    const [amount, setAmount] = useState(0)
-    // declare state variable to handle toggle the render of the receipt
-    const [receiptDisplay, setReceiptDisplay] = useState(false)
+export default function Transfer() {
+  
+  // declare useRef hook to track the state change of the user input in the Amount text box
+  const valueRef = useRef(0)
+  // declare state variable to keep track to submited amount value
+  const [amount, setAmount] = useState(0)
+  // declare state variable to handle toggle the render of the receipt
+  const [receiptDisplay, setReceiptDisplay] = useState(false)
+  
+  // Event Handlers
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setAmount(valueRef.current.value)
+    // console.log(valueRef.current.value)
+    setReceiptDisplay(true)
+  };
 
-    return (
-        <>
-            <TransferForm fromAddress={sourceAccount} toAddress={destinationAccount} valueRef={valueRef} onSubmit={(_newAmount) => setAmount(_newAmount)} displayReceipt={() => setReceiptDisplay(true)}/>
-            {receiptDisplay && <Receipt mockData={recieptData}/>}
-        </>
-        )
+  // Testing function
+  // To check if valueRef being updated
+  const amountChange = (value) => {
+    console.log(value.current.value)
+  }
+
+  return (
+    <div>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Transfer
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="fromAddress"
+                  label="From Address"
+                  autoFocus
+                  defaultValue={sourceAccount}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="ToAddress"
+                  label="To Address"
+                  defaultValue={destinationAccount}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="amount"
+                  label="Amount"
+                  inputRef={valueRef}
+                  onChange={() => amountChange(valueRef)}
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Submit
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+
+    {receiptDisplay && <Receipt mockData={recieptData}/>}
+  </div>
+  );
 }
-
-export default Transfer
