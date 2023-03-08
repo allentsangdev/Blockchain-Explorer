13cbe16f02de6be9a4d745567aa694e05a53e8fb
allentsangdev <allentsang04@gmail.com>
2023-03-07 23:16:31 -0500

    added the Receipt component and mock data

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,23 @@ import './App.css';
 import Transfer from './Transfer';
 import Receipt from './Receipt';
 
+// Defining the mock data
+// With only one object for testing
+const recieptData =
+  {
+      transactionHash: "0x1367409cddde9a7c8571d34f935adcb2a50214f2afbb151bb16eaf8847dda2ff",
+      blockHash: "0xf32c44c730609dbbf6d5ba0ab7d9747460b8ab4f3d1c81d822cc10a6e48f2613",
+      blockNumber: "10",
+      from: "0x6dC70bEa16f1ef94A7350989ca5413a2E180860f",
+      to: "0x03d0cf3f4A832C8E2c224BaA4a049110F39E630F",
+      gasUsed: "21000"
+  }
+
 function App() {
   return (
     <div className="App">
       <Transfer/>
-      <Receipt/>
+      <Receipt mockData={recieptData}/>
     </div>
   );
 }
--- src/components/Receipt.jsx
+++ src/components/Receipt.jsx
@@ -1,6 +1,62 @@
-function Receipt() {
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
+
+
+
+function Receipt(props) {
     return (
-        <></>
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Typography component="h1" variant="h5">
+                            Receipt
+                        </Typography>
+                        
+                        <Box component="form" noValidate sx={{ mt: 3 }}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12}>
+                                    <Typography component="p" variant="p"> Transaction Hash: {props.mockData.transactionHash} </Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Typography component="p" variant="p"> Block Hash: {props.mockData.blockHash} </Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Typography component="p" variant="p"> Block Number: {props.mockData.blockNumber} </Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Typography component="p" variant="p"> From: {props.mockData.from} </Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Typography component="p" variant="p"> To: {props.mockData.to} </Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Typography component="p" variant="p"> Gas Used: {props.mockData.gasUsed}</Typography>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    
+                    </Box>
+            </Container>
+        </ThemeProvider>
     )
 }
 
