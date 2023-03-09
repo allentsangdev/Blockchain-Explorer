2a18c15e09af819867a06603d6282973ad571add
allentsangdev <allentsang04@gmail.com>
2023-03-09 07:04:55 -0500

    updating Navbar: 1. dark mode 2. drawer

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,31 @@ import Transfer from './Transfer';
 import Navbar from './Navbar';
 import CssBaseline from '@mui/material/CssBaseline';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useState } from 'react';
 
-const theme = createTheme({
+// Dark Mode and Light Mode Material UI theme setting
+const darkThemeMode = createTheme({
   palette: {
     mode: 'dark',
   },
 });
 
+const lightThemeMode = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
+
+// Rendering App Component
 function App() {  
+  const [darkTheme, setTheme] = useState(true)
+  const switchTheme = (_theme) => setTheme(_theme)
+
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={darkTheme ? darkThemeMode : lightThemeMode}>
       <CssBaseline/>
       <div className="App">
-        <Navbar/>
+        <Navbar switchTheme={switchTheme}/>
         <Transfer/>
       </div>
     </ThemeProvider>
