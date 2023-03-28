1918c45de12636c72da86a2cf1127becf5e86899
allentsangdev <allentsang04@gmail.com>
2023-03-28 04:37:40 -0400

    update wallet component

--- src/components/Wallet.jsx
+++ src/components/Wallet.jsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import axios from 'axios'
+import { useEffect, useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
@@ -9,16 +11,29 @@ import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 
 // Defining the mock data
 // With only one object for testing
+/*
 const walletData =
   {
       address: "0x1367409cddde9a7c8571d34f935adcb2a50214f2afbb151bb16eaf8847dda2ff",
       balance: "100 ETH"
   }
+*/
 
 export default function Wallet() {
   
+  const apiUri = 'https://blockchain-explorer-api-z0rh.onrender.com/account/balance/0x6dC70bEa16f1ef94A7350989ca5413a2E180860f'
+  const [walletData, setWalletData] = useState(0)
+
+  // component did mount hook to fetch data from api 
+  useEffect(() => {
+    axios.get(apiUri).then(response => {
+      setWalletData(response.data)
+      console.log(response.data)
+    })
+  }, [])
+
   return (
-    <div>
+    <>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
           <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center',}}>
@@ -31,7 +46,7 @@ export default function Wallet() {
             <Box component="form" noValidate sx={{ mt: 3 }}>
               <Grid container spacing={2}>
                 <Grid item xs={12}>
-                  <Typography component="p" variant="p"> Address: {walletData.address} </Typography>
+                  <Typography component="p" variant="p"> Address: 0x6dC70bEa16f1ef94A7350989ca5413a2E180860f </Typography>
                 </Grid>
                 <Grid item xs={12}>
                   <Typography component="p" variant="p"> Balance: {walletData.balance} </Typography>
@@ -45,7 +60,7 @@ export default function Wallet() {
             </Box>
         </Box>
       </Container>
-    </div>
+    </>
     
   );
 }
