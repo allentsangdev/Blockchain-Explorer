025e6ee8c41272989d148f293f612eb51e2cd7e2
allentsangdev <allentsang04@gmail.com>
2023-04-10 22:42:23 -0400

    make wallet dynamic

--- src/components/Wallet.jsx
+++ src/components/Wallet.jsx
@@ -8,33 +8,30 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import Alert from '@mui/material/Alert';
 
-// Defining the mock data
-// With only one object for testing
-/*
-const walletData =
-  {
-      address: "0x1367409cddde9a7c8571d34f935adcb2a50214f2afbb151bb16eaf8847dda2ff",
-      balance: "100 ETH"
-  }
-*/
 
 export default function Wallet() {
-  
-  //const apiUri = 'https://blockchain-explorer-api-z0rh.onrender.com/account/balance/0x6dC70bEa16f1ef94A7350989ca5413a2E180860f'
-  const apiUri = 'http://localhost:4000/account/balance/0x6dC70bEa16f1ef94A7350989ca5413a2E180860f'
-  const [walletData, setWalletData] = useState(null)
 
-  // component did mount hook to fetch data from api 
+  //const apiUri = 'https://blockchain-explorer-api-z0rh.onrender.com/account/balance/0x6dC70bEa16f1ef94A7350989ca5413a2E180860f'
+  const [walletData, setWalletData] = useState(null)
+  // component did mount hook to fetch the first node addresses from api. This address will be the default address for the wallet
   useEffect(() => {
-    axios.get(apiUri).then(response => {
-      setWalletData(response.data)
-      console.log(response.data)
+    axios.get('http://localhost:4000/account/addresses').then(response => {
+      const nodeAddress = response.data[0]
+      const apiUri = `http://localhost:4000/account/balance/${nodeAddress}`
+      axios.get(apiUri).then(response => {
+        setWalletData(response.data)
+        console.log(response.data)
+      })
+    }).catch(error => {
+      console.error(error)
     })
   }, [])
-
+  
   return (
     <>
+      <Alert severity="info">Note that this wallet using the first accounnt from your local blockchain node as default account</Alert>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
           <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center',}}>
